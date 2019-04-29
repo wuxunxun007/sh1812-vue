7e81131a6102dd1f94a1843124adcd5e8ba21068
wudaxun <522864637@qq.com>
2019-04-29 14:44:43 +0800

    首页使用轮播图组件

--- src/views/home/index.vue
+++ src/views/home/index.vue
@@ -1,12 +1,30 @@
 <template>
   <div class="box">
     <header class="header">首页</header>
-    <div class="content">首页</div>
+    <div class="content">
+      <div class="banner">
+        <mt-swipe :auto="4000">
+          <mt-swipe-item>1</mt-swipe-item>
+          <mt-swipe-item>2</mt-swipe-item>
+          <mt-swipe-item>3</mt-swipe-item>
+        </mt-swipe>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+import { Swipe, SwipeItem } from 'mint-ui'
+Vue.use(Swipe, SwipeItem)
 export default {
 
 }
 </script>
+
+<style lang="scss">
+.banner {
+  height: 1.5rem;
+  border-bottom: 1px solid #ccc;
+}
+</style>
