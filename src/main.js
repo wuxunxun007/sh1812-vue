71c43230636745655929456f83bb722b3b22e12f
wudaxun <522864637@qq.com>
2019-04-29 14:33:31 +0800

    配置UI库

--- src/main.js
+++ src/main.js
@@ -3,7 +3,8 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
-
+import MintUI from 'mint-ui'
+Vue.use(MintUI)
 Vue.config.productionTip = false
 
 /* eslint-disable no-new */
